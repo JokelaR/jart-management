cbd9fa55521a95d2b65e6f675fad2ae91bc86bb9
JokelaR <jokelaromi@gmail.com>
2024-12-23 02:24:24 +0000

    add automatic height/width attributes to gallery images

--- django-project/mediaserver/templatetags/media_utility.py
+++ django-project/mediaserver/templatetags/media_utility.py
@@ -9,3 +9,13 @@ def invert(value):
 @register.filter(name='underscore_to_space')
 def underscoreToSpace(value: str):
     return value.replace('_', ' ')
+
+@register.simple_tag(name='image_size')
+def image_size(width: int, height: int) -> str:
+    max_width = 966
+
+    ratio = width / height
+    width = min(width, max_width)
+    new_height = width / ratio
+
+    return f'height={new_height} width={width}'
