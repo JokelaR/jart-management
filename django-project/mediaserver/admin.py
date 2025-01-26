60da84d93a167f5332ae716d94427ce5db05f61c
JokelaR <jokelaromi@gmail.com>
2025-01-26 11:17:25 +0000

    Add rich admin view for Tags

--- django-project/mediaserver/admin.py
+++ django-project/mediaserver/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from .models import Media, Gallery, Tag
+
+class OrphanTagFilter(admin.SimpleListFilter):
+    title = 'Orphaned Tags'
+    parameter_name = 'orphaned'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('yes', 'Orphan')
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'yes':
+            return queryset.filter(tag_count=0)
+        return queryset
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['namespace', 'tagname', 'tag_count']
+    list_filter = ['namespace', ('description', admin.EmptyFieldListFilter), OrphanTagFilter]
+    search_fields = ['tagname', 'namespace']
+
 # Register your models here.
 admin.site.register(Media)
 admin.site.register(Gallery)
-admin.site.register(Tag)
+admin.site.register(Tag, TagAdmin)
