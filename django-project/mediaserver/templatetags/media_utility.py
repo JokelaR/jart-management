3f5c28fb81f53a6951a9cbf16223ece47ecb386e
JokelaR <jokelaromi@gmail.com>
2026-02-09 04:58:17 +0000

    mouseum specific style expansions

--- django-project/mediaserver/templatetags/media_utility.py
+++ django-project/mediaserver/templatetags/media_utility.py
@@ -8,7 +8,7 @@ def invert(value):
 
 @register.filter(name='underscore_to_space')
 def underscoreToSpace(value: str):
-    return value.replace('_', ' ')
+    return value.replace('_', ' ').lstrip('0123456789 ')
 
 @register.simple_tag(name='image_size')
 def image_size(width: int, height: int) -> str:
