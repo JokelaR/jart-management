7c77b0da48ce8ce56e8a8d72cf08dcf214ba9733
JokelaR <jokelaromi@gmail.com>
2026-05-01 15:47:04 +0000

    type annotations for admin view

--- django-project/mediaserver/admin.py
+++ django-project/mediaserver/admin.py
@@ -1,35 +1,38 @@
 from django.contrib import admin
 from .models import Media, Gallery, Tag
+from django.http import HttpRequest
+from django.db.models import QuerySet
+
 
 class OrphanTagFilter(admin.SimpleListFilter):
     title = 'Orphaned Tags'
     parameter_name = 'orphaned'
 
-    def lookups(self, request, model_admin):
+    def lookups(self, request: HttpRequest, model_admin):
         return [
             ('yes', 'Orphan')
         ]
 
-    def queryset(self, request, queryset):
+    def queryset(self, request: HttpRequest, queryset: QuerySet[Tag]):
         if self.value() == 'yes':
             return queryset.filter(tag_count=0)
         return queryset
 
 
-class TagAdmin(admin.ModelAdmin):
+class TagAdmin(admin.ModelAdmin[Tag]):
     list_display = ['namespace', 'tagname', 'tag_count']
     list_filter = ['namespace', ('description', admin.EmptyFieldListFilter), OrphanTagFilter]
     search_fields = ['tagname', 'namespace']
     actions = ['check_tag_users']
 
-    def get_actions(self, request):
+    def get_actions(self, request: HttpRequest):
         actions = super().get_actions(request)
         if 'delete_selected' in actions:
             del actions['delete_selected']
         return actions
 
     @admin.action(description='Check tag users')
-    def check_tag_users(self, request, queryset):
+    def check_tag_users(self, request: HttpRequest, queryset: QuerySet[Tag]):
         for tag in queryset:
             tag.count_uses()
 
